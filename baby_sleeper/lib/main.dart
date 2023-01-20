c49d6e0189e7f2d764e0e4ecb9b36c681b6efb7e
caferoman <daduki@gmail.com>
2023-01-20 16:05:49 +0900

    two carousel menus with different category

--- baby_sleeper/lib/main.dart
+++ baby_sleeper/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'audio_main.dart';
 
-final List<String> imgList = [
+final List<String> pororoImgList = [
   'https://daduki.github.io/images/pororo01.jpeg',
   'https://daduki.github.io/images/pororo02.jpeg',
   'https://daduki.github.io/images/pororo03.jpeg',
@@ -26,6 +26,15 @@ final List<String> imgList = [
   'https://daduki.github.io/images/pororo17.jpeg',
   'https://daduki.github.io/images/pororo18.jpeg',
   'https://daduki.github.io/images/pororo19.jpeg',
+];
+
+final List<String> tayoImgList = [
+  'https://daduki.github.io/images/tayo001.jpg',
+  'https://daduki.github.io/images/tayo002.jpg',
+  'https://daduki.github.io/images/tayo003.jpg',
+  'https://daduki.github.io/images/tayo004.jpg',
+  'https://daduki.github.io/images/tayo005.jpg',
+  'https://daduki.github.io/images/tayo006.jpg',
 ];
 
 void main() => runApp(MyApp());
@@ -55,7 +64,7 @@ class MyHomePage extends StatelessWidget {
           bottom: TabBar(
             tabs: <Widget>[
               Tab(icon: Icon(Icons.tag_faces), text: '음뽀'),
-              Tab(icon: Icon(Icons.abc), text: '메뉴2'),
+              Tab(icon: Icon(Icons.emoji_transportation), text: '타요'),
               Tab(icon: Icon(Icons.info), text: '메뉴3'),
               Tab(icon: Icon(Icons.dangerous), text: '메뉴4'),
               Tab(icon: Icon(Icons.earbuds), text: 'Music'),
@@ -63,52 +72,13 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
         body: TabBarView(children: <Widget>[
-          ImageCapture(),
           FullscreenSliderDemo(),
+          FullscreenSliderDemo2(),
           Container(color: Colors.amber[50]),
           Container(color: Colors.amber[100]),
           AudioMain(),
         ]),
       ),
-    );
-  }
-}
-
-class ImageCapture extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return PageView(
-      children: <Widget>[
-        Image.asset("assets/pororo01.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo02.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo03.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo04.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo05.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo06.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo07.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo08.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo09.jpeg", width: 400, height: 600),
-        Image.asset("assets/pororo10.jpeg", width: 400, height: 600),
-      ],
-    );
-  }
-}
-
-class CarouselPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          child: CarouselSlider(
-        options: CarouselOptions(),
-        items: imgList
-            .map((item) => Container(
-                  child: Center(
-                      child:
-                          Image.network(item, fit: BoxFit.cover, width: 1000)),
-                ))
-            .toList(),
-      )),
     );
   }
 }
@@ -127,13 +97,42 @@ class FullscreenSliderDemo extends StatelessWidget {
               enlargeCenterPage: false,
               // autoPlay: false,
             ),
-            items: imgList
+            items: pororoImgList
                 .map((item) => Container(
                       child: Center(
                           child: Image.network(
                         item,
                         fit: BoxFit.fitWidth,
-                        height: height,
+                      )),
+                    ))
+                .toList(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class FullscreenSliderDemo2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) {
+          final double height = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              // autoPlay: false,
+            ),
+            items: tayoImgList
+                .map((item) => Container(
+                      child: Center(
+                          child: Image.network(
+                        item,
+                        fit: BoxFit.fitWidth,
                       )),
                     ))
                 .toList(),
